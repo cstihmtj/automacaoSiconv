be051801f46eecf6a591ec425f24e39446df3b96
OSS HMTJ <csti@hmtj.org.br>
2024-11-26 15:10:51 -0300

    Correção volta segundada rodada!

--- automacoes/dp.js
+++ automacoes/dp.js
@@ -122,7 +122,10 @@ const lancarPagamento = async (row, countLines, page, anexo, anexoPath) => {
             await page.click("#tbodyrow > tr > td > div > a");
             await delay(500)
             await Promise.all([
-                await page.goto(process.env.HOSTDP3)
+                await page.goto(process.env.HOSTDP3),
+                await page.waitForSelector("input[name='Incluir Documento de Liquidação']", { visible: true }),
+                await page.click("input[name='Incluir Documento de Liquidação']"),
+                await delay(500)
             ])
         }
         if (anexo && await fileExist(anexoPath, row[11]) || !anexo && row[3].length == 11) {
